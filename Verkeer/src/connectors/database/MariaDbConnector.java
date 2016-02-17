f42b1131cfff8d739b8a6bf504c6e5c678764580
RobinWeymans <Robin@192.168.1.122>
2016-02-17 17:47:25 +0100

    Type changes MariaDbConnector insert methods

--- Verkeer/src/connectors/database/MariaDbConnector.java
+++ Verkeer/src/connectors/database/MariaDbConnector.java
@@ -7,6 +7,7 @@ package connectors.database;
 
 import connectors.ProviderEntry;
 import connectors.DataEntry;
+import connectors.RouteEntry;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
@@ -26,7 +27,9 @@ public class MariaDbConnector implements IDbConnector{
     private final String DATABASE = "verkeer1";
     private final String URL = "jdbc:mariadb://"+IP+":"+PORT+"/"+DATABASE;
     
-    private final String INSERT_TTE = "INSERT INTO traveltime (routeID, providerID, traveltime) values ( ?, ?, ?);";
+    private final String INSERT_DE = "INSERT INTO trafficdata (routeID, providerID, timestamp, traveltime) values ( ?, ?, ?, ?);";
+    private final String INSERT_PE = "INSERT INTO providers (id, name) values ( ?, ?);";
+    private final String INSERT_RE = "INSERT INTO routesINSERT_DE (id, length, name, startlat, startlong, endlat, endlong) values ( ?, ?, ?, ?, ?, ?, ?);";
     
     private Connection getConnection(){
         try{
@@ -37,7 +40,7 @@ public class MariaDbConnector implements IDbConnector{
         }catch (ClassNotFoundException e){
             
         }catch (SQLException e){
-            
+            System.err.println("getConnection() FOUT!");
         }
         return connection;
     }
@@ -45,12 +48,42 @@ public class MariaDbConnector implements IDbConnector{
     @Override
     public void insert(DataEntry entry)  {
         try(Connection conn = getConnection()){
-            PreparedStatement p = conn.prepareStatement(INSERT_TTE);
+            PreparedStatement p = conn.prepareStatement(INSERT_DE);
+            p.setInt( 1, entry.getProvider().getId());
+            p.setInt( 2, entry.getRoute().getId());
+            p.setDate(3, entry.getTimestamp());
+            p.setDouble(4, entry.getTravelTime());
+            p.executeQuery();
         } catch (SQLException ex) {
-            
+            System.err.println("insert DataEntry FOUT!");
         }
     }
-
+    public void insert(ProviderEntry entry){
+        try(Connection conn = getConnection()){
+            PreparedStatement p = conn.prepareStatement(INSERT_PE);
+            p.setInt    ( 1, entry.getId());
+            p.setString ( 2, entry.getName());
+            p.executeQuery();
+        } catch (SQLException ex) {
+            System.err.println("insert DataEntry FOUT!");
+        }
+    }
+    public void insert(RouteEntry entry){
+        try(Connection conn = getConnection()){
+            PreparedStatement p = conn.prepareStatement(INSERT_RE);
+            p.setInt    ( 1, entry.getId());
+            p.setInt    ( 2, entry.getLenght());
+            p.setString (3, entry.getName());
+            p.setDouble (4, entry.getStartCoordinateLatitude());
+            p.setDouble (5, entry.getStartCoordinateLongitude());
+            p.setDouble (6, entry.getEndCoordinateLatitude());
+            p.setDouble (7, entry.getEndCoordinateLongitude());
+            p.executeQuery();
+        } catch (SQLException ex) {
+            System.err.println("insert DataEntry FOUT!");
+        }
+    }
+    
     @Override
     public ProviderEntry findByName(String name) {
         return null;
