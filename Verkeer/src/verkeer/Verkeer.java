a95723529e411b2e6ad579352acd535a4f6cbb20
RobinWeymans <Robin@192.168.1.122>
2016-02-21 21:10:38 +0100

    Empty ResultSet checks in MariaDbConnector Removal test code in Verkeer.java

--- Verkeer/src/verkeer/Verkeer.java
+++ Verkeer/src/verkeer/Verkeer.java
@@ -1,6 +1,7 @@
 
 package verkeer;
 
+import connectors.ProviderEntry;
 import connectors.RouteEntry;
 import connectors.database.MariaDbConnector;
 
@@ -11,17 +12,7 @@ import connectors.database.MariaDbConnector;
 public class Verkeer {
     public static void main(String[] args) {
         
-        RouteEntry entry = new RouteEntry();
-        entry.setName("Test traject");
-        entry.setLenght(123);
-        entry.setIdealTravelTime(555);
-        entry.setStartCoordinateLatitude(12);
-        entry.setStartCoordinateLongitude(34);
-        entry.setEndCoordinateLatitude(56);
-        entry.setEndCoordinateLongitude(78);
         
-        MariaDbConnector m = new MariaDbConnector();
-        m.insert(entry);
         
     }
     
