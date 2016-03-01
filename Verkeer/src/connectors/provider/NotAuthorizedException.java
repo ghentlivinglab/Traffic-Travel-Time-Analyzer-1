d5d712892d13ecb5343a0eb6ca74f9176578e260
Simon Backx <simonbbackx@gmail.com>
2016-03-01 10:21:35 +0100

    Cleaned Waze connector

---
+++ Verkeer/src/connectors/provider/NotAuthorizedException.java
@@ -0,0 +1,25 @@
+package connectors.provider;
+
+public class NotAuthorizedException extends Exception {
+
+    public NotAuthorizedException() {
+        super();
+    }
+
+    public NotAuthorizedException(String message) {
+        super(message);
+    }
+
+    public NotAuthorizedException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public NotAuthorizedException(Throwable cause) {
+        super(cause);
+    }
+
+    public NotAuthorizedException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
+        super(message, cause, enableSuppression, writableStackTrace);
+    }
+
+}
