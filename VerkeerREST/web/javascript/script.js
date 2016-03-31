134a0c43844eb14949a3f485a148c24feda5e505
Jarno De Meyer <jarno.de.meyer@gmail.com>
2016-03-31 15:29:49 +0200

    Unified the language in script.js
    Comments are all in English

--- VerkeerREST/web/javascript/script.js
+++ VerkeerREST/web/javascript/script.js
@@ -1,56 +1,77 @@
-// Gebruikte globale variabelen
-// Worden in het begin gevraagd aan de API en ingevuld, eenmalig.
+/****************************
+ * global variables
+ * API fills these once only
+ ****************************/
 var providers = [];
 var routes = [];
 var events = [];
 
-// Kleine uitbreiding op mustache
-// Om een  template te renderen die in onze script tags gedefineerd staan
-// Gebeurt met Mustache
-// renderTemplate('route', {name: "...", ...});
+
+
+/****************************
+ * Small extension of Mustache
+ * renders a template from a script-tag
+ * renderTemplate('route', {name: "...", ...});
+ ****************************/
 Mustache.renderTemplate = function (template_name, view) {
 	var template = document.getElementById("template-"+template_name).innerHTML;
 	Mustache.parse(template); // Speeding things up in the future
  	return Mustache.render(template, view);
 }
 
+
+/****************************
+ * toggles the display of the overview panel
+ ****************************/
 function togglePanel(){
-	if($("#dashboard").css("left")==="250px"){
-		$("#dashboard").animate({left:-550});
-		$(".collapse").children().attr({"src":"images/arrow-right.png","alt":">"});
-	} else{
-		$("#dashboard").animate({left:250});
-		$(".collapse").children().attr({"src":"images/arrow-left.png","alt":"<"});
+	if($("#dashboard").css("left")==="250px"){ // panel is shown
+		$("#dashboard").animate({left:-550}); // hides panel to the left
+		$(".collapse").children().attr({"src":"images/arrow-right.png","alt":">"}); // changes the display of the button
+	} else{ // panel is not shown
+		$("#dashboard").animate({left:250}); // shows panel
+		$(".collapse").children().attr({"src":"images/arrow-left.png","alt":"<"}); // changes the display of the button
 	}
 
 }
 
-// leest de key-value paren uit in de URL
-// eg:
-// bij de url http://test.com/?food=banana&drink=beer
-// zal de methode getQueryVariable("food") "banana" teruggeven
-function getQueryVariable(variable)
-{
-       var query = window.location.search.substring(1);
-       var vars = query.split("&");
+/****************************
+ * reads the key-value pairs from the location-URL
+ * eg: for the URL http://test.com/?food=banana&drink=beer the method getQueryVariable("food") returns "banana"
+ ****************************/
+function getQueryVariable(variable){
+       var query = window.location.search.substring(1); // removes '?'
+       var vars = query.split("&"); // splits the key-value pairs
        for (var i=0;i<vars.length;i++) {
-               var pair = vars[i].split("=");
-               if(pair[0] == variable){return pair[1];}
+               var pair = vars[i].split("="); // splits keys from values
+               if(pair[0] == variable){return pair[1];} //returns value when requested key has been found
        }
-       return(false);
+       return false; // key not found
 }
 
+
+/****************************
+ * runs when DOM-tree is finished
+ ****************************/
 $(document).ready( function(){
-	// Enkel op Windows -> scrollbar
+	
+	// runs the nice-scrollbar script on non-Mac devices
 	if (navigator.userAgent.indexOf('Mac OS X') == -1) {
 		$("#dashboard .content").niceScroll({zindex:999,cursorcolor:"#CCCCCC"});
 	}
+	
+	// adds a click listener to the collapse-button
 	$(".collapse").click(togglePanel);
 
+	// initialises dashboard
 	Dashboard.init();
 });
 
+/****************************
+ * runs when DOM-tree is finished and all objects from it are loaded
+ ****************************/
 $(window).load( function(){
+	
+	// checks if mapView or overview has to be displayed
 	if(getQueryVariable("mapView")==="true"){
 		togglePanel();
 	}
