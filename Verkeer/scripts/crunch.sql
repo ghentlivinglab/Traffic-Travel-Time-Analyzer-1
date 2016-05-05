2d27fc71e4aec51aab27c9a659cfe78ded2bfb83
Simon Backx <simonbbackx@gmail.com>
2016-05-05 18:29:29 +0200

    Cruncher fixed

--- Verkeer/scripts/crunch.sql
+++ Verkeer/scripts/crunch.sql
@@ -1,25 +1,27 @@
 use verkeer1;
 SET SQL_SAFE_UPDATES = 0;
 
-create table temp as 
-	SELECT 
-		x.id as id,
-		(SELECT 
-				AVG(y.traveltime)
-			FROM
-				trafficdata y
-			WHERE
-						x.providerID = y.providerID
-					AND x.routeID = y.routeID
-					AND y.timestamp between x.timestamp - INTERVAL 60 DAY and x.timestamp
-					AND ABS(MINUTE(TIMEDIFF(TIME(x.timestamp), TIME(y.timestamp)))) < 10
-					AND WEEKDAY(timestamp) = WEEKDAY(MAX(x.timestamp))) AS avgdag
-	FROM
-		trafficdata x
-	where x.avgtraveltimeday = 0
-	group by x.id;
+create TEMPORARY table temp as 
+SELECT 
+        x.id as id,
+(SELECT 
+                AVG(y.traveltime)
+            FROM
+                trafficdata y
+            WHERE
+                        x.providerID = y.providerID
+                    AND x.routeID = y.routeID
+                    AND y.timestamp between x.timestamp - INTERVAL 60 DAY and x.timestamp
+                    AND abs(TIMESTAMPDIFF(minute, time(x.timestamp), TIME(y.timestamp))) < 15
+                    AND WEEKDAY(timestamp) = WEEKDAY(x.timestamp)
+            ) AS avgdag
+    FROM
+        trafficdata x
+    where x.avgtraveltimeday = 0 or x.avgtraveltimeday is null
+    group by x.id;
+update trafficdata x
 
-update trafficdata a
-set a.avgtraveltimeday=(select avgdag from temp b where a.id = b.id);
+set x.avgtraveltimeday=(select avgdag from temp b where x.id = b.id)
+where x.avgtraveltimeday = 0 or x.avgtraveltimeday is null;
 
 drop table temp;
